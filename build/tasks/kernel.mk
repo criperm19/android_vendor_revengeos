42389653033379edfdfbc39a535e26c59508a054
criperm19 <96837536+criperm19@users.noreply.github.com>
2023-10-18 19:34:28 -0300

    Update kernel.mk

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -139,12 +139,28 @@ else
         $(warning * BoardConfig.mk file                                    *)
         $(warning **********************************************************)
         # $(error "NO KERNEL CONFIG")
-    else
-        #$(info Kernel source found, building it)
-        FULL_KERNEL_BUILD := true
-        KERNEL_BIN := $(TARGET_PREBUILT_INT_KERNEL)
-    endif
 endif
+ ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(LINEAGE_BUILDTYPE)),)
+            $(error "PREBUILT KERNEL IS NOT ALLOWED ON OFFICIAL BUILDS!")
+        else
+            ifneq ($(TARGET_FORCE_PREBUILT_KERNEL),)
+                $(warning **********************************************************)
+                $(warning * Kernel source found and configuration was defined,     *)
+                $(warning * but prebuilt kernel is being forced.                   *)
+                $(warning * While this is likely intentional,                      *)
+                $(warning * it is NOT SUPPORTED WHATSOEVER.                        *)
+                $(warning * Generated kernel headers may not align with            *)
+                $(warning * the ABI of kernel you're including.                    *)
+                $(warning * Please unset TARGET_FORCE_PREBUILT_KERNEL              *)
+                $(warning * to build the kernel from source.                       *)
+                $(warning **********************************************************)
+                FULL_KERNEL_BUILD := false
+                KERNEL_BIN := $(TARGET_PREBUILT_KERNEL)
+            else
+                FULL_KERNEL_BUILD := true
+                KERNEL_BIN := $(TARGET_PREBUILT_INT_KERNEL)
+            endif
+        endif
 
 ifeq ($(FULL_KERNEL_BUILD),true)
 
